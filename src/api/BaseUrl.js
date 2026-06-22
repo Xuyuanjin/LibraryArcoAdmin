499148dc234d8f97b524d6c9640d3f30d80507aa
lndev <wifi2017vip@gmail.com>
2026-06-22 11:04:33 +0800

    Update API base URL

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -1,6 +1,6 @@
 export function bUrl() {
-    let baseUrl = "http://localhost:8081"
-    // let baseUrl = "https://api.lnpine.lol"
+    // let baseUrl = "http://localhost:8081"
+    let baseUrl = "https://api.1024777.xyz"
     return baseUrl;
 }
 
